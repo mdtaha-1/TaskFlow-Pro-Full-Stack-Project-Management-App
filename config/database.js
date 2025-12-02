a1e8aa92c25289803cacf34a7e79299efc807791
mdtaha-1 <mohammedtaha534@gmail.com>
2025-12-02 07:41:25 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,12 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://studymate:studymate123@cluster0.w3venfq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'api key');
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
     process.exit(1);
   }
 };
+
 
 export default connectDB;
